86a30fd12cf9d808ba6865ddc11f8453b3487087
MercyAndati <andatimercy8@gmail.com>
2025-07-24 22:44:27 +0300

    new update

--- client/src/pages/Notifications.jsx
+++ client/src/pages/Notifications.jsx
@@ -1,7 +1,7 @@
 "use client"
 import { createContext, useContext, useState, useEffect } from "react"
 import api from "../services/api"
-import { useAuth } from "./AuthContext"
+import { useAuth } from '../contexts/AuthContext';
 import io from "socket.io-client"
 import { Bell } from "lucide-react"
 
